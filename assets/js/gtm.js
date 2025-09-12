181735c7209bccebe5973358e326ed91d1c8e160
daniloarfaria <daniloantonio@agenciaark.com.br>
2025-09-11 23:43:52 -0300

    v.1.3.4

--- assets/js/gtm.js
+++ assets/js/gtm.js
@@ -3,7 +3,7 @@
   w[l]=w[l]||[];
   w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
   var f=d.getElementsByTagName(s)[0], j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-  j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+  j.async=true; j.src='https://stape.solucionne.com.br/gtm.js?id='+i+dl;
   f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TJ88RS5C');
 
